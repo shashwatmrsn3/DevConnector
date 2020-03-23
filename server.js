0e37e2ec410e0d67b662f214ef5aefde69b91068
shashwatmrsn <shashwatmrsn3@gmail.com>
2020-03-23 14:06:27 +0545

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.get('/',(req,res)=>res.send('api running'));
+
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/posts',require('./routes/api/posts'));
+app.use('/api/profile',require('./routes/api/profile'));
+
+
+
+app.listen(5000,()=>console.log('Server started on port 5000'));
