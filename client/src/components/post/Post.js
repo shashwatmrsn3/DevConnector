bd0c5fd3f422517358cb64581320d815e479928d
shashwatmrsn <shashwatmrsn3@gmail.com>
2020-04-11 14:56:38 +0545

    added comment view and delete functionality

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import PostItem from '../posts/PostItem';
 
 import {getPost, getPosts} from '../../actions/post';
 import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 
 const Post = ({getPost,post:{post,loading},match}) => {
     
@@ -13,6 +14,11 @@ const Post = ({getPost,post:{post,loading},match}) => {
     },[getPost]);
     return ( loading || post === null ? <Spinner/> : <Fragment><PostItem post={post} showActions={false}/>
     <CommentForm postId = {post._id}/>
+    <div className='comments'>
+        {post.comments.map(comment => (
+            <CommentItem key = {comment._id} comment = {comment} postId = {post._id}/>
+        ))}
+    </div>
     </Fragment>);
 }
 
